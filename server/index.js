4be91bd112aac2999594afb6b81404d7207c876d
sarthakastic <iamsg26@gmail.com>
2023-01-20 03:18:44 +0530

    initial setup

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import postRoutes from "../server/routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://sarthakastic26:26may2000@cluster0.ixlpljy.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5001;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
